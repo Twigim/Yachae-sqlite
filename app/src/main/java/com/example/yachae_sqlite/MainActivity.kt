2c3d368e7687e71fa058d47eb2150b1cd8b3c2fe
seungeun-Leee <leesuel0379@gmail.com>
2022-07-26 13:49:21 +0900

    [FIX] 식단한줄평 DB 수정

--- app/src/main/java/com/example/yachae_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/yachae_sqlite/MainActivity.kt
@@ -1,58 +1,57 @@
 package com.example.yachae_sqlite
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    private val tableName:String = "noteData"
+    private val tableName: String = "noteData"
 
-    private lateinit var dbHelper: SQLiteOpenHelper
+    private lateinit var dbManager: DBManager
     private lateinit var database: SQLiteDatabase
-    private lateinit var layout: LinearLayout
 
+    //lateinit var noteRecycler: RecyclerView
+    lateinit var textView: TextView
+
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dbManager = DBManager(this, "noteDB", null, 1)
+        database = dbManager.readableDatabase
 
-        databaseCreate()    //데이터베이스생성
-        createTable()       //테이블 생성
+        textView = findViewById(R.id.tvContent)
 
-        database = dbHelper.readableDatabase
+        var cursor: Cursor
+        cursor = database.rawQuery("SELECT * FROM noteData;", null)
 
+        var num:Int = 0
+        while(cursor.moveToNext()){
+            var str_content = cursor.getString(cursor.getColumnIndex("content")).toString()
 
+            var tvContent: TextView = TextView(this)
+            tvContent.text = str_content
+            tvContent.setPadding(10,10,10,0)
 
-        //val newNotepad: ImageButton = findViewById(R.id.newNoteButton_main)
-        //이미지버튼 클릭
+            num++
+        }
+        cursor.close()
+        database.close()
+        dbManager.close()
+
         val newNotepad: Button = findViewById(R.id.newNoteButton_main)
         newNotepad.setOnClickListener {
-            //NewNotepadActivity 클래스 실행 (새 메모)
-            startActivity(Intent(this, NewNotepadActivity::class.java))
+            startActivity(Intent(this, NoteReg::class.java))
             finish()
         }
-
-
-    }
-
-    private fun createTable() {
-        database.execSQL(
-            "create table if not exists ${tableName}(" +
-                    "_id integer PRIMARY KEY autoincrement," +
-                    "title text," +
-                    "content text," +
-                    "time text)"
-        )
-    }
-
-    private fun databaseCreate() {
-        // 데이터베이스 생성 | 쓰기 가능한 상태로 설정
-        dbHelper = DatabaseHelper(this)
-        database = dbHelper.writableDatabase
     }
 }
---
+++ app/src/main/java/com/example/yachae_sqlite/NoteContents.kt
@@ -0,0 +1,46 @@
+package com.example.yachae_sqlite
+
+import android.annotation.SuppressLint
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class NoteContents:AppCompatActivity() {
+
+    lateinit var dbManager: DBManager
+    lateinit var database: SQLiteDatabase
+
+    lateinit var tvContent: TextView
+
+    lateinit var str_content: String
+
+    @SuppressLint("Range")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvContent = findViewById(R.id.tvContent)
+
+        val intent = intent
+        str_content = intent.getStringExtra("intent_content").toString()
+
+        dbManager = DBManager(this, "noteDB", null, 1)
+        database = dbManager.readableDatabase
+
+        var cursor: Cursor
+        cursor = database.rawQuery("SELECT * FROM noteData WHERE '"+str_content+"';", null)
+
+        if(cursor.moveToNext()){
+            str_content = cursor.getString((cursor.getColumnIndex("content"))).toString()
+        }
+
+        cursor.close()
+        database.close()
+        dbManager.close()
+
+        tvContent.text = str_content
+    }
+
+}
