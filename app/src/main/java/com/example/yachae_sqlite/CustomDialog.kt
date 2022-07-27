f487ad091926760b39f8713a95b3990deaa4382e
SeYoun Lee <lsy20140@naver.com>
2022-07-27 21:23:57 +0900

    [DESIGN] 로고, 회원가입/로그인 타이틀 수정

--- app/src/main/java/com/example/yachae_sqlite/CustomDialog.kt
+++ app/src/main/java/com/example/yachae_sqlite/CustomDialog.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 
 
 class CustomDialog: DialogFragment() {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.dialog_corner);
+        getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.dialog_corner)
+
 
         return inflater.inflate(R.layout.custom_dialog, container, false)
     }
@@ -20,6 +23,8 @@ class CustomDialog: DialogFragment() {
         val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
         val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
         dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+
     }
 
 }
