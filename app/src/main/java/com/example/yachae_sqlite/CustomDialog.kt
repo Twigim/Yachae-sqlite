bd221228d8bcf4ecd9164a3714c01236a7b28ce3
SeYoun Lee <lsy20140@naver.com>
2022-07-27 22:25:31 +0900

    [FIX] bottom sheet 내용 전체 나타나게 수정

--- app/src/main/java/com/example/yachae_sqlite/CustomDialog.kt
+++ app/src/main/java/com/example/yachae_sqlite/CustomDialog.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 
 
 class CustomDialog: DialogFragment() {
 
+    lateinit var btn_close_dialog : Button
+    lateinit var goto_veg_type_info : TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.dialog_corner)
@@ -21,9 +24,22 @@ class CustomDialog: DialogFragment() {
     override fun onStart() {
         super.onStart()
         val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
-        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
         dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
 
+        // dialog 닫기
+        btn_close_dialog = dialog!!.findViewById(R.id.btn_close_dialog) as Button
+
+        btn_close_dialog.setOnClickListener {
+            dialog!!.dismiss()
+        }
+
+        // 채식 유형 정보 bottom sheet 보여주기기
+        goto_veg_type_info = dialog!!.findViewById(R.id.goto_veg_type_info) as TextView
+
+        val bottomSheetFragment = VegTypeBottomSheetFragment()
+        goto_veg_type_info.setOnClickListener {
+            bottomSheetFragment.show(childFragmentManager,bottomSheetFragment.tag )
+        }
 
     }
 
