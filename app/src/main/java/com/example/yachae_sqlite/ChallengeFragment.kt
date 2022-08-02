cda56641407ee62efa44dbd3111af59a12b2ae3a
emilywin825 <emilywin825@naver.com>
2022-08-02 03:15:09 -0700

    [FEAT] challengeFragment.kt 오류..

--- app/src/main/java/com/example/yachae_sqlite/ChallengeFragment.kt
+++ app/src/main/java/com/example/yachae_sqlite/ChallengeFragment.kt
@@ -18,100 +18,87 @@ import com.google.android.material.chip.ChipGroup
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 class ChallengeFragment: Fragment {
     lateinit var NextButton: Button
     lateinit var PreviousButton: Button
     lateinit var CurrentDate: TextView
     lateinit var gridView: GridView
+    lateinit var dbOpenHelper: DBOpenHelper
     var calendar = Calendar.getInstance(Locale.KOREAN)
-    //lateinit var context: Context
     var dateFormat = SimpleDateFormat("yyyy MMMM", Locale.KOREAN)
     lateinit var myGridAdapter: MyGridAdapter
     lateinit var alertDialog: AlertDialog
+    lateinit var chip_name : String
+    lateinit var chip_milk:Chip
     var dates: MutableList<Date> = ArrayList()
-    var insert = false
-    lateinit var chipName: String
-    lateinit var chipGroup: ChipGroup
-    lateinit  var dbopenhelper: DBOpenHelper
-    lateinit var db: SQLiteDatabase
 
-    lateinit var dbManager: DBManager
+    var chip:Boolean=false
+
+    var insert:Boolean = false
+    lateinit var chipname: String
 
     var initChallenge : Boolean = false
-    constructor(context: Context?) : super(context) {}
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
         IntializeLayout()
         SetUpCalendar()
+
+        //전달로 이동 버튼
         PreviousButton!!.setOnClickListener { //-1한 월을 넣어줌
             calendar.add(Calendar.MONTH, -1)
-            SetUpCalendar()
-        }
+            SetUpCalendar() }
+
+        //다음달로 이동 버튼
         NextButton!!.setOnClickListener { //+1한 월을 넣어줌
             calendar.add(Calendar.MONTH, 1)
-            SetUpCalendar()
-        }
+            SetUpCalendar() }
+
+        //dbOpenHelper=DBOpenHelper(this,"chips",null,1)
+
         gridView!!.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id ->
                 val builder = AlertDialog.Builder(context)
                 val addView =
                     LayoutInflater.from(parent.context).inflate(R.layout.add_newevent_layout, null)
-                Log.d("Gridview", "Gridview 눌렸음")
+
                 val AddEvent = addView.findViewById<Button>(R.id.addevent)
                 val btn_close = addView.findViewById<Button>(R.id.btn_close_dialog)
-                val chipgroup = addView.findViewById<ChipGroup>(R.id.chipgroup)
-                val chip_milk = addView.findViewById<Chip>(R.id.chip_milk)
+                val chipgroup = addView.findViewById(R.id.chipgroup) as ChipGroup
+                chip_milk = addView.findViewById(R.id.chip_milk)
                 val chip_dairyproduct = addView.findViewById<Chip>(R.id.chip_dairyproduct)
                 val chip_vegetable = addView.findViewById<Chip>(R.id.chip_vegetable)
                 val chip_egg = addView.findViewById<Chip>(R.id.chip_egg)
                 val chip_fish = addView.findViewById<Chip>(R.id.chip_fish)
                 val chip_meat = addView.findViewById<Chip>(R.id.chip_meat)
                 val chip_fowls = addView.findViewById<Chip>(R.id.chip_fowls)
+
+                Log.d("Gridview", "Gridview 눌렸음")
+
                 builder.setView(addView)
                 alertDialog = builder.create()
                 alertDialog.show()
-                AddEvent.isClickable = false
-                chipName = chip_milk.text.toString()
 
-                /*   chip_milk.setOnClickListener(new OnClickListener()
-                        {
-                            @Override
-                            public void onClick(View v){
-                                if(chip_milk.isChecked()==true){
-                                Log.d("chip_milk","chip_milk 성공");
-                                insert=true;
-                                //insert=dbOpenHelper.insertData(chipName);
-                                //텍스트로 insert}
-                            }
-                        });*/AddEvent.setOnClickListener(object : View.OnClickListener {
-                var str_name = ""
-                override fun onClick(v: View) {
-                    Log.d("기록하기 버튼", "눌렸습니다.")
-                    //SaveEvent(EventName.getText().toString(),EventName.getText().toString());
-                    if (chipgroup.checkedChipId == R.id.chip_milk) {
-                        Log.d("CHIP_MILK", "milk 눌림")
-                        str_name = chip_milk.text.toString()
+
+                AddEvent.setOnClickListener(object : View.OnClickListener {
+                    override fun onClick(v: View) {
+                        /*
+                           if (chip_milk.isChecked()==true) {
+                               insert=dbOpenHelper.chipinsertData(chipname)
+                               Log.d("CHIP_MILK", "milk 눌림")
+                               *//*if(insert==true){
+                        Toast.makeText(this@CustomCalendarActivity, "기록 성공!!", Toast.LENGTH_SHORT).show()}*//*
+                }*/
+                        Log.d("기록하기 버튼", "눌렸습니다.")
+                        view.setBackgroundResource(R.drawable.stamp3black)
+                        alertDialog.dismiss()
                     }
-                    //db = dbopenhelper!!.writableDatabase
-                    //db.execSQL("INSERT INTO chipName VALUES('$str_name');")
-                    //db.close()
-                    view.setBackgroundResource(R.drawable.stamp3black)
-                    alertDialog.dismiss()
-                }
-            })
+                })
+
+                //취소 버튼 클릭시
                 btn_close.setOnClickListener {
                     Log.d("CLOSEBTN", "취소 버튼 눌림")
                     alertDialog.dismiss()
                 }
             }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
     }
 
     /*private void SaveEvent(String event, String date,String month,String year){
@@ -120,7 +107,7 @@ class ChallengeFragment: Fragment {
         dbOpenHelper.SaveEvent(event,date,month,year,database);
     }*/
     private fun IntializeLayout() {
-        val inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val inflater =requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.calendar_layout, this)
         PreviousButton = view.findViewById(R.id.previousBtn)
         NextButton = view.findViewById(R.id.nextBtn)
@@ -146,14 +133,13 @@ class ChallengeFragment: Fragment {
             //1일씩 늘린 날짜로 변경
             monthCalendar.add(Calendar.DAY_OF_MONTH, 1)
         }
-        myGridAdapter = MyGridAdapter(dates, calendar)
+        myGridAdapter = MyGridAdapter(context!!, dates, calendar)
         gridView!!.adapter = myGridAdapter
     }
 
     companion object {
         private const val MAX_CALENDAR_DAYS = 42
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -172,7 +158,7 @@ class ChallengeFragment: Fragment {
     override fun onStart() {
         super.onStart()
 
-        dbManager = DBManager(context)
+        var dbManager = DBManager(context)
         initChallenge = dbManager!!.existsColumnInTable("users", "veg_type")
 
         if (initChallenge == true) {
